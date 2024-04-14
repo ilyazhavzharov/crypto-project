bbc6f6ebd89817c2094778bb895a8d3544ef590d
ILYA ZHAVZHAROV <zhavzharov.info@gmail.com>
2024-04-15 00:25:56 +0300

    finish with general section

---
+++ js/animations.js
@@ -0,0 +1,35 @@
+AOS.init();
+
+// SCROLL
+SmoothScroll({
+  // Время скролла 400 = 0.4 секунды
+  animationTime: 2500,
+  // Размер шага в пикселях
+  stepSize: 75,
+
+  // Дополнительные настройки:
+
+  // Ускорение
+  accelerationDelta: 80,
+  // Максимальное ускорение
+  accelerationMax: 2,
+
+  // Поддержка клавиатуры
+  keyboardSupport: true,
+  // Шаг скролла стрелками на клавиатуре в пикселях
+  arrowScroll: 50,
+
+  // Pulse (less tweakable)
+  // ratio of "tail" to "acceleration"
+  pulseAlgorithm: true,
+  pulseScale: 4,
+  pulseNormalize: 1,
+
+  // Поддержка тачпада
+  touchpadSupport: true,
+});
+
+// new TypeIt("#general__title", {
+//   speed: 120,
+//   waitUntilVisible: true,
+// }).go();
