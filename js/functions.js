7de7996a14bde56e1ce1d94f096fd6d15f2d625b
ILYA ZHAVZHAROV <zhavzharov.info@gmail.com>
2024-04-18 15:22:51 +0300

    add burger menu functional

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,33 @@
+$(".header__burger").click(function () {
+  $(".menu__list").toggleClass("active");
+  $(".header__button").toggleClass("active-button");
+  $(".header__burger").toggleClass("header__burger-active");
+  $(".burger__line-1").toggleClass("burger__line-1-active");
+  $(".burger__line-2").toggleClass("burger__line-2-active");
+  $(".burger__line-3").toggleClass("burger__line-3-active");
+  $("body").toggleClass("menu-open");
+});
+
+$(".menu__link").click(function () {
+  $(".header__button").toggleClass("active-button");
+  $(".menu__list").toggleClass("active");
+  $("body").toggleClass("menu-open");
+  $(".header__burger").toggleClass("header__burger-active");
+  $(".burger__line-1").toggleClass("burger__line-1-active");
+  $(".burger__line-2").toggleClass("burger__line-2-active");
+  $(".burger__line-3").toggleClass("burger__line-3-active");
+});
+
+$(".header__button").click(function () {
+  $(".header__button").toggleClass("active-button");
+  $(".menu__list").toggleClass("active");
+  $("body").toggleClass("menu-open");
+  $(".header__burger").toggleClass("header__burger-active");
+  $(".burger__line-1").toggleClass("burger__line-1-active");
+  $(".burger__line-2").toggleClass("burger__line-2-active");
+  $(".burger__line-3").toggleClass("burger__line-3-active");
+});
+
 function copyText() {
   var text = document.getElementById("textToCopy");
   var range = document.createRange();
